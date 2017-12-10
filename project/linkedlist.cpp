1080ec8e0ef2db48632488dcaefbbc7777c3a7b5
HinJ <21600695@handong.edu>
2017-12-10 14:15:34 +0000

    linkedlist.cpp6

--- project/linkedlist.cpp
+++ project/linkedlist.cpp
@@ -84,3 +84,123 @@ void linkedlist::InsertToFront(int val)
     IntegerNode* inode = new IntegerNode(val, pHead);
     pHead = inode;
 }
+
+void linkedlist::InsertToBack(int val)
+{
+    if(isEmpty())
+    {
+        InsertToFront(val);
+    }
+    else
+    {
+        Node* last_node = find_prev_node(nullptr);
+        
+        IntegerNode* inode = new IntegerNode(val, nullptr);
+        last_node->set_next(inode);
+    }
+}
+
+int linkedlist::RemoveFromFront_Integer()
+{
+    int temp = 0;
+    if(!isEmpty())
+    {
+        Node* cur = pHead;
+        IntegerNode* inode = static_cast<IntegerNode*>(cur);
+        temp = inode -> get_value();
+        
+        pHead = cur->get_next();
+        
+        delete cur;
+    }
+    return temp;
+}
+
+int linkedlist::RemoveFromBack_Integer()
+{
+    int temp = 0;
+    if(!isEmpty())
+    {
+        if(pHead->get_next() == nullptr)
+        {
+            IntegerNode* inode = static_cast<IntegerNode*>(pHead);
+            temp = inode->get_value();
+            
+            delete inode;
+            
+            pHead = nullptr;
+        }
+        else
+        {
+            Node* last_node = find_prev_node(nullptr);
+            Node* prev_node = find_prev_node(last_node);
+            prev_node -> set_next(nullptr);
+            IntegerNode* inode = static_cast<IntegerNode*>(last_node);
+            temp = inode -> get_value();
+            
+            delete inode;
+        }
+    }
+    return temp;
+}
+
+void linkedlist::InsertToFront(char* val)
+{
+    
+}
+
+void linkedlist::InsertToBack(char* val)
+{
+    
+}
+
+char* linkedlist::RemoveFromFront_String()
+{
+    char* temp;
+    return temp;
+}
+
+char* linkedlist::RemoveFromBack_String()
+{
+    char* temp;
+    return temp;
+}
+
+bool linkedlist::isEmpty()
+{
+    return pHead == nullptr? true:false;
+}
+
+Node* linkedlist::find_prev_node(Node* next)
+{
+    Node* cur = pHead;
+    if(next == cur)
+    {
+        return nullptr;
+    }
+    
+    while(cur->get_next() != next && cur != nullptr )
+    {
+        cur = cur->get_next();
+    }
+    
+    return cur;
+}
+
+void linkedlist::print(std::ostream& os)
+{
+    for(Node* cur = pHead; cur != nullptr; cur = cur->get_value())
+    {
+        if(cur->get_node_type() == INT_NODE)
+        {
+            IntegerNode* node = static_cast<IntegerNode*>(cur);
+            os << node->get_value()<<" ";
+        }
+        else if(cur->get_node_type() == STRING_NODE)
+        {
+            StringNode* node = static_cast<StringNode*>(cur);
+            os << node -> get_value() << " ";
+        }
+    }
+    os << std::endl;
+}
